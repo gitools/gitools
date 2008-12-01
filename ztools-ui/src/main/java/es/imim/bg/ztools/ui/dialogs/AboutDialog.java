852b65d61d420295c72428110c0142bf094e7d6d
chris-zen <chrispz@gmail.com>
2008-12-01 12:05:20 +0000

    ui: AboutDialog accents removed.
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@104 1b512f91-3386-4a98-81e7-b8836ddf8916

--- ztools-ui/src/main/java/es/imim/bg/ztools/ui/dialogs/AboutDialog.java
+++ ztools-ui/src/main/java/es/imim/bg/ztools/ui/dialogs/AboutDialog.java
@@ -57,8 +57,8 @@ public class AboutDialog extends JDialog {
 		//FIXME: It should load this text from a resource
 		infoPane.setText("<h1>" + appName + "</h1>"
 						+ "<p>Version " + appVersion + "</p>"
-						+ "<p>Written by Christian Pérez-Llamas</p>"
-						+ "<p>Biomedical Genomics Laboratory<br>Parc de Recerca Biomèdica de Barcelona (PRBB)</p>");
+						+ "<p>Written by Christian Perez-Llamas</p>"
+						+ "<p>Biomedical Genomics Laboratory<br>Parc de Recerca Biomedica de Barcelona (PRBB)</p>");
 		
 		JButton acceptBtn = new JButton("OK");
 		acceptBtn.setMargin(new Insets(0, 30, 0, 30));
