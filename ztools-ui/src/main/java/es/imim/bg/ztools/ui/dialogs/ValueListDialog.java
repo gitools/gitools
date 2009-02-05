1866cda8da6d012defea77c4410515adb1db69ca
michphip <michael.p.schroeder@gmail.com>
2009-02-05 18:06:00 +0000

    ui: debug in valueListDialog & AbstactModel
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@164 1b512f91-3386-4a98-81e7-b8836ddf8916

--- ztools-ui/src/main/java/es/imim/bg/ztools/ui/dialogs/ValueListDialog.java
+++ ztools-ui/src/main/java/es/imim/bg/ztools/ui/dialogs/ValueListDialog.java
@@ -6,7 +6,6 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.SampleModel;
 
 import java.util.ArrayList;
 import java.util.List;
