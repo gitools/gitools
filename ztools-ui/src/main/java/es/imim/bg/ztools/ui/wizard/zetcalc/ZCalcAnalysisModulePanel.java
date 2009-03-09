1f218251c74a7c57a06933bb9425cd3a2edf492c
chris-zen <chrispz@gmail.com>
2009-03-09 00:19:31 +0000

    ui-1.4: minor changes.
    git-svn-id: https://bg.upf.edu/svn/gitools/branches/1.4@213 1b512f91-3386-4a98-81e7-b8836ddf8916

--- ztools-ui/src/main/java/es/imim/bg/ztools/ui/wizard/zetcalc/ZCalcAnalysisModulePanel.java
+++ ztools-ui/src/main/java/es/imim/bg/ztools/ui/wizard/zetcalc/ZCalcAnalysisModulePanel.java
@@ -75,7 +75,7 @@ public class ZCalcAnalysisModulePanel extends JPanel {
         
         contentPanel1.setLayout(new java.awt.BorderLayout());
 
-        panelTitle.setText("Module");
+        panelTitle.setText("Modules");
         panelTitle.setFont(new java.awt.Font(panelTitle.getFont().getName(), Font.BOLD, 18));
         contentPanel1.add(panelTitle, java.awt.BorderLayout.NORTH);
 
