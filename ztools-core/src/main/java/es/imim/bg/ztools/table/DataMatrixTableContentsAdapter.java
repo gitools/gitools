01f07a22c189cc5276fcf52c4d4207ca56e17adb
Sergi Hernandez <sergi.hernandez@upf.edu>
2009-03-23 14:59:00 +0000

    core-1.4: DataMatrixTableContentsAdapter added at element package
    git-svn-id: https://bg.upf.edu/svn/gitools/branches/1.4@261 1b512f91-3386-4a98-81e7-b8836ddf8916

---
+++ ztools-core/src/main/java/es/imim/bg/ztools/table/DataMatrixTableContentsAdapter.java
@@ -0,0 +1,65 @@
+package es.imim.bg.ztools.table;
+
+import es.imim.bg.ztools.model.DataMatrix;
+import es.imim.bg.ztools.table.element.IElementAdapter;
+import es.imim.bg.ztools.table.element.basic.DoubleElementAdapter;
+import es.imim.bg.ztools.table.element.basic.StringElementAdapter;
+
+public class DataMatrixTableContentsAdapter implements ITableContents {
+
+	private DataMatrix data;
+	
+	public DataMatrixTableContentsAdapter(DataMatrix data) {
+		this.data = data;
+	}
+	
+	public Object getCell(int row, int column) {
+		return data.getData().get(row, column);
+	}
+
+	public Object getCellValue(int row, int column, int index) {
+		return data.getData().get(row, column);
+	}
+
+	public Object getCellValue(int row, int column, String id) {
+		return data.getData().get(row, column);
+	}
+
+	public Object getColumn(int index) {
+		return data.getColNames()[index];
+	}
+
+	public int getColumnCount() {
+		return data.getColNames().length;
+	}
+
+	public Object getRow(int index) {
+		return data.getRowNames()[index];
+	}
+
+	public int getRowCount() {
+		return data.getRowNames().length; 
+	}
+
+	public void setCellValue(int row, int column, int index, Object value) {
+		data.getData().set(row, column, (Double) value);
+	}
+
+	public void setCellValue(int row, int column, String id, Object value) {
+		data.getData().set(row, column, (Double) value);
+	}
+
+	public IElementAdapter getCellAdapter() {
+		//FIXME: the adapter must come with the resource 
+		return new DoubleElementAdapter();
+	}
+
+	public IElementAdapter getColumnAdapter() {
+		return new StringElementAdapter();
+	}
+
+	public IElementAdapter getRowAdapter() {
+		return new StringElementAdapter();
+	}
+
+}
