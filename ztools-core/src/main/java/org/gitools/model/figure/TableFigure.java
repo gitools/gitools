8674339ef43794a4e0bfa818386540fc1b105d13
Sergi Hernandez <sergi.hernandez@upf.edu>
2009-06-08 18:42:56 +0000

    gitools-core: ITableView added
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@380 1b512f91-3386-4a98-81e7-b8836ddf8916

--- ztools-core/src/main/java/org/gitools/model/figure/TableFigure.java
+++ ztools-core/src/main/java/org/gitools/model/figure/TableFigure.java
@@ -1,23 +1,19 @@
 package org.gitools.model.figure;
 
-import org.gitools.model.table.IMatrixView;
+import org.gitools.model.table.ITableView;
 
-public abstract class TableFigure extends Figure {
-	/**
-	 * Its a Figure associated to a table.
-	 */
-	protected IMatrixView matrixView;
+public class TableFigure extends Figure {
+	
+	protected ITableView tableView;
 
 	public TableFigure() {
-		super();
 	}
 
-	public IMatrixView getTable() {
-		return matrixView;
+	public ITableView getTableView() {
+		return tableView;
 	}
-
-	public void setTable(IMatrixView matrixView) {
-		this.matrixView = matrixView;
+	
+	public void setTableView(ITableView tableView) {
+		this.tableView = tableView;
 	}
-
 }
---
+++ ztools-core/src/main/java/org/gitools/model/table/ITableView.java
@@ -0,0 +1,5 @@
+package org.gitools.model.table;
+
+public interface ITableView {
+
+}
