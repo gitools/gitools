42e2f747dcba6da0edb9255e986da34b22236d0b
chris-zen <chrispz@gmail.com>
2009-09-04 17:06:44 +0000

    ztools-ui: working on EnrichmentAnalysisWizard
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@527 1b512f91-3386-4a98-81e7-b8836ddf8916

---
+++ ztools-ui/src/main/java/org/gitools/ui/wizard/common/NameFilterPage.java
@@ -0,0 +1,21 @@
+package org.gitools.ui.wizard.common;
+
+import javax.swing.JComponent;
+
+import org.gitools.ui.wizard.AbstractWizardPage;
+
+public class NameFilterPage extends AbstractWizardPage {
+
+	private NameFilterPanel panel;
+
+	public NameFilterPage() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public JComponent createControls() {
+		panel = new NameFilterPanel();
+		return panel;
+	}
+
+}
