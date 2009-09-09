58560e0bc15472b4e97e05eebb00367441b4d60a
Sergi Hernandez <sergi.hernandez@upf.edu>
2009-09-09 16:50:40 +0000

    gitools-core: working on binary color scale / decorator
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@538 1b512f91-3386-4a98-81e7-b8836ddf8916

---
+++ ztools-core/src/main/java/edu/upf/bg/colorscale/BinaryColorScale.java
@@ -0,0 +1,38 @@
+package edu.upf.bg.colorscale;
+
+import java.awt.Color;
+
+public class BinaryColorScale extends AbstractColorScale {
+
+	private double cutoff = 1.0;
+	
+	public BinaryColorScale( 
+			double minPoint, 
+			double maxPoint, 
+			double cutoff,
+			Color minColor,
+			Color maxColor) {
+		super(minPoint, maxPoint);
+		this.cutoff = cutoff;
+		this.minColor = minColor;
+		this.maxColor = maxColor;
+	}
+	
+	public BinaryColorScale() {
+		super(0.0, 1.0);
+	}
+	
+	@Override
+	public Color valueColor(double value) {
+		if (Double.isNaN(value))
+			return notANumberColor;
+		else if (value > maxPoint || value == Double.POSITIVE_INFINITY)
+			return posInfinityColor;
+		else if (value < minPoint || value == Double.NEGATIVE_INFINITY)
+			return negInfinityColor;
+		
+		return value >= cutoff ? 
+				maxColor: minColor;
+	}
+
+}
