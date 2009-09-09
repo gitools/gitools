6611c9faf5843027bb9f87eae079462618c75a56
Sergi Hernandez <sergi.hernandez@upf.edu>
2009-09-09 17:19:02 +0000

    gitools-core: nonSignificantColor in BinaryColorScale
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@540 1b512f91-3386-4a98-81e7-b8836ddf8916

--- ztools-core/src/main/java/edu/upf/bg/colorscale/BinaryColorScale.java
+++ ztools-core/src/main/java/edu/upf/bg/colorscale/BinaryColorScale.java
@@ -2,20 +2,28 @@ package edu.upf.bg.colorscale;
 
 import java.awt.Color;
 
+import edu.upf.bg.colorscale.util.ColorConstants;
+
 public class BinaryColorScale extends AbstractColorScale {
 
 	private double cutoff = 1.0;
+	
+	private Color nonSignificantColor  = 
+		ColorConstants.nonSignificantColor;
+	
 	
 	public BinaryColorScale( 
 			double minPoint, 
 			double maxPoint, 
 			double cutoff,
 			Color minColor,
-			Color maxColor) {
+			Color maxColor,
+			Color nonSignificantColor) {
 		super(minPoint, maxPoint);
 		this.cutoff = cutoff;
 		this.minColor = minColor;
 		this.maxColor = maxColor;
+		this.nonSignificantColor = nonSignificantColor;
 	}
 	
 	public BinaryColorScale() {
@@ -35,4 +43,21 @@ public class BinaryColorScale extends AbstractColorScale {
 				maxColor: minColor;
 	}
 
+	public void setNonSignificantColor(Color nonSignificantColor) {
+		this.nonSignificantColor = nonSignificantColor;
+	}
+
+	public Color getNonSignificantColor() {
+		return nonSignificantColor;
+	}
+
+	public double getCutoff() {
+		return cutoff;
+	}
+
+	public void setCutoff(double cutoff) {
+		this.cutoff = cutoff;
+	}
+
+	
 }
