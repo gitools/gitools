b788b16845c1a7f1620fe9aa8fcb0a6afee99d72
chris-zen <chrispz@gmail.com>
2009-09-09 18:14:59 +0000

    gitools-biomart: working on ws client
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@541 1b512f91-3386-4a98-81e7-b8836ddf8916

--- gitools-biomart/src/main/java/org/gitools/biomart/Main.java
+++ gitools-biomart/src/main/java/org/gitools/biomart/Main.java
@@ -1,8 +1,32 @@
 package org.gitools.biomart;
 
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.gitools.biomart.client.BioMartException_Exception;
+import org.gitools.biomart.client.BioMartSoapService;
+import org.gitools.biomart.client.Mart;
+import org.gitools.biomart.client.MartServiceSoap;
+
 public class Main {
 
 	public static void main(String[] args) {
+		BioMartSoapService service = new BioMartSoapService();
+		
+		Iterator<QName> ports = service.getPorts();
+		while (ports.hasNext())
+			System.out.println(ports.next());
+		
+		MartServiceSoap port = service.getBioMartSoapPort();
+		try {
+			List<Mart> marts = port.getRegistry();
+			for (Mart mart : marts)
+				System.out.println(mart);
+		} catch (BioMartException_Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
