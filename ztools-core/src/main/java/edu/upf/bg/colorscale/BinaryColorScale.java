f610bc19782fb51f6e044584f50ed3df3d6f6d75
Sergi Hernandez <sergi.hernandez@upf.edu>
2009-09-18 14:12:30 +0000

    gitools-core:BinarySacle now is using BinaryCutoffCmp
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@553 1b512f91-3386-4a98-81e7-b8836ddf8916

--- ztools-core/src/main/java/edu/upf/bg/colorscale/BinaryColorScale.java
+++ ztools-core/src/main/java/edu/upf/bg/colorscale/BinaryColorScale.java
@@ -2,27 +2,26 @@ package edu.upf.bg.colorscale;
 
 import java.awt.Color;
 
+import org.gitools.datafilters.BinaryCutoffFilter;
+import org.gitools.datafilters.BinaryCutoffFilter.BinaryCutoffCmp;
+
 public class BinaryColorScale extends AbstractColorScale {
 
 	private double cutoff = 1.0;
-	
-	public BinaryColorScale( 
-			double minPoint, 
-			double maxPoint, 
-			double cutoff,
-			Color minColor,
-			Color maxColor,
-			Color nonSignificantColor) {
+	private BinaryCutoffCmp cutoffCmp = BinaryCutoffFilter.EQ;
+
+	public BinaryColorScale(double minPoint, double maxPoint, double cutoff,
+			Color minColor, Color maxColor, Color nonSignificantColor) {
 		super(minPoint, maxPoint);
 		this.cutoff = cutoff;
 		this.minColor = minColor;
 		this.maxColor = maxColor;
 	}
-	
+
 	public BinaryColorScale() {
 		super(0.0, 1.0);
 	}
-	
+
 	@Override
 	public Color valueColor(double value) {
 		if (Double.isNaN(value))
@@ -31,11 +30,11 @@ public class BinaryColorScale extends AbstractColorScale {
 			return posInfinityColor;
 		else if (value < minPoint || value == Double.NEGATIVE_INFINITY)
 			return negInfinityColor;
-		
-		return value >= cutoff ? 
-				maxColor: minColor;
+
+		boolean isSig = cutoffCmp.compare(value, cutoff);
+		return isSig ? maxColor : minColor;
 	}
-	
+
 	public double getCutoff() {
 		return cutoff;
 	}
@@ -43,5 +42,13 @@ public class BinaryColorScale extends AbstractColorScale {
 	public void setCutoff(double cutoff) {
 		this.cutoff = cutoff;
 	}
-	
+
+	public BinaryCutoffCmp getCutoffCmp() {
+		return cutoffCmp;
+	}
+
+	public void setCutoffCmp(BinaryCutoffCmp cutoffCmp) {
+		this.cutoffCmp = cutoffCmp;
+	}
+
 }
