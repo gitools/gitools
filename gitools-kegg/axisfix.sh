4798c6c1c7fdb4a2b67d307e53d837fde9608b25
chris-zen <chrispz@gmail.com>
2010-09-28 16:40:08 +0000

    Gitools KEGG module prepared.
    git-svn-id: https://bg.upf.edu/svn/gitools/trunk@955 1b512f91-3386-4a98-81e7-b8836ddf8916

---
+++ gitools-kegg/axisfix.sh
@@ -0,0 +1,1 @@
+perl src/main/resources/wsdl/axisfix.pl target/generated-sources/axistools/org/gitools/kegg/soap/KEGGBindingStub.java
---
+++ gitools-kegg/src/main/java/org/gitools/kegg/App.java
@@ -0,0 +1,30 @@
+package org.gitools.kegg;
+
+import java.rmi.RemoteException;
+import javax.xml.rpc.ServiceException;
+import org.gitools.kegg.soap.Definition;
+import org.gitools.kegg.soap.KEGGLocator;
+import org.gitools.kegg.soap.KEGGPortType;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) throws ServiceException, RemoteException {
+		KEGGLocator locator = new KEGGLocator();
+		KEGGPortType serv = locator.getKEGGPort();
+
+		String query = "path:eco00020";
+		String[] results = serv.get_genes_by_pathway(query);
+
+		for (int i = 0; i < results.length; i++) {
+			System.out.println(results[i]);
+		}
+
+		Definition[] org = serv.list_organisms();
+		for (Definition def : org)
+			System.out.println(def.getEntry_id() + ", " + def.getDefinition());
+	}
+}
