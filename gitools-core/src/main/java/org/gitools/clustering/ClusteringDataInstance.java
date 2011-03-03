2e66953752614d065182a055c4dca5aa90c39e18
chris-zen <chrispz@gmail.com>
2011-03-03 10:26:17 +0000

    Clustering core work.
    git-svn-id: https://bg.upf.edu/svn/gitools/desktop/trunk@1083 1b512f91-3386-4a98-81e7-b8836ddf8916

--- gitools-core/src/main/java/org/gitools/clustering/ClusteringDataInstance.java
+++ gitools-core/src/main/java/org/gitools/clustering/ClusteringDataInstance.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2011 Universitat Pompeu Fabra.
- *
+ *  Copyright 2011 chris.
+ * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *
+ * 
  *       http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,15 +17,10 @@
 
 package org.gitools.clustering;
 
-/** Generic data interface for clustering methods */
-public interface ClusteringData {
+public interface ClusteringDataInstance {
 
-	/** Return the number of elements in the data */
-	int getSize();
+	Class<?> getValueClass(int attribute);
 
-	/** Returns the label associated with the instance at <index> */
-	String getLabel(int index);
-
-	/** Returns an instance of the data */
-	ClusteringDataInstance getInstance(int index);
+	Object getValue(int attribute);
+	<T> T getTypedValue(int attribute, Class<T> valueClass);
 }
