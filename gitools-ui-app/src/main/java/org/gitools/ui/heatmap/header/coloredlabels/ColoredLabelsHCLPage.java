b7e2dbaf425b22920d37007bbf66cba9ef397ade
xrafael <xavi.rafael@gmail.com>
2011-03-24 17:54:18 +0000

    Clustering: Information per tree level
    git-svn-id: https://bg.upf.edu/svn/gitools/desktop/trunk@1132 1b512f91-3386-4a98-81e7-b8836ddf8916

--- gitools-ui-app/src/main/java/org/gitools/ui/heatmap/header/coloredlabels/ColoredLabelsHCLPage.java
+++ gitools-ui-app/src/main/java/org/gitools/ui/heatmap/header/coloredlabels/ColoredLabelsHCLPage.java
@@ -28,7 +28,10 @@ import javax.swing.Action;
 import javax.swing.KeyStroke;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import org.gitools.clustering.HierarchicalClusteringResults;
+import org.gitools.clustering.method.value.ClusterUtils;
 import org.gitools.heatmap.header.HeatmapColoredLabelsHeader;
 import org.gitools.ui.IconNames;
 import org.gitools.ui.platform.AppFrame;
@@ -51,7 +54,7 @@ public class ColoredLabelsHCLPage extends AbstractWizardPage {
 
 
     /** Creates new form ColoredClustersConfigPage */
-    public ColoredLabelsHCLPage(HeatmapColoredLabelsHeader header) {
+    public ColoredLabelsHCLPage(final HeatmapColoredLabelsHeader header) {
 		super();
 
 		this.header = header;
@@ -73,7 +76,27 @@ public class ColoredLabelsHCLPage extends AbstractWizardPage {
 
 		int treeDepth = header.getHCLResults().getNewickTree().getTreeDepth();
 		SpinnerModel sm = new SpinnerNumberModel(header.getHCLCurrentDepth(), 0, treeDepth, 1);
+
 		this.levelSpin.setModel(sm);
+
+		int numClusters = header.getHCLResults().getNewickTree().getRoot().getLeaves(
+				(Integer) levelSpin.getValue()).size();
+
+		lblClusterLevel.setText(String.valueOf(numClusters));
+
+		lblTreeDepth.setText(String.valueOf(treeDepth));
+
+		lblMaxClusters.setText(String.valueOf(header.getHCLResults().getNewickTree().getRoot().getLeaves().size()));
+
+		levelSpin.addChangeListener(new ChangeListener() {
+			@Override public void stateChanged(ChangeEvent e) {
+					
+					int numClusters = header.getHCLResults().getNewickTree().getRoot().getLeaves(
+							(Integer) levelSpin.getValue()).size();
+
+					lblClusterLevel.setText(String.valueOf(numClusters));
+			}
+		});
 
 		treePanel.setLayout(new BorderLayout());
 		
@@ -142,10 +165,16 @@ public class ColoredLabelsHCLPage extends AbstractWizardPage {
         treePanel = new javax.swing.JPanel();
         zoomInBtn = new javax.swing.JButton();
         zoomOutBtn = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        lblTreeDepth = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lblClusterLevel = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        lblMaxClusters = new javax.swing.JLabel();
 
         setBorder(null);
 
-        jLabel3.setText("Draw clusters from tree level:");
+        jLabel3.setText("Draw clusters for tree level:");
 
         levelSpin.setModel(new javax.swing.SpinnerNumberModel(Integer.valueOf(1), Integer.valueOf(0), null, Integer.valueOf(1)));
 
@@ -156,45 +185,87 @@ public class ColoredLabelsHCLPage extends AbstractWizardPage {
         treePanel.setLayout(treePanelLayout);
         treePanelLayout.setHorizontalGroup(
             treePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 630, Short.MAX_VALUE)
+            .addGap(0, 744, Short.MAX_VALUE)
         );
         treePanelLayout.setVerticalGroup(
             treePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 290, Short.MAX_VALUE)
+            .addGap(0, 153, Short.MAX_VALUE)
         );
+
+        jLabel1.setText("Maximum tree levels:");
+
+        lblTreeDepth.setText("0");
+
+        jLabel2.setText("Resulting number of clusters :");
+
+        lblClusterLevel.setText("0");
+
+        jLabel4.setText("Maximum number of clusters :");
+
+        lblMaxClusters.setText("0");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(treePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(treePanel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(18, 18, 18)
-                        .addComponent(levelSpin, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(84, 84, 84)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 223, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(levelSpin, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel2)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblMaxClusters, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addComponent(lblClusterLevel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(lblTreeDepth, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jLabel1))
+                        .addGap(18, 345, Short.MAX_VALUE)
                         .addComponent(zoomInBtn)
+                        .addGap(28, 28, 28)
+                        .addComponent(zoomOutBtn)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(zoomOutBtn)))
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel3)
-                        .addComponent(levelSpin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(levelSpin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblClusterLevel)
+                                .addGap(18, 18, 18)
+                                .addComponent(lblTreeDepth))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(53, 53, 53)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(jLabel4)
+                                    .addComponent(lblMaxClusters)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(93, 93, 93)
+                        .addComponent(jLabel1)))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(zoomOutBtn)
                     .addComponent(zoomInBtn))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
                 .addComponent(treePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -204,7 +275,13 @@ public class ColoredLabelsHCLPage extends AbstractWizardPage {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup colorGroup;
     private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel lblClusterLevel;
+    private javax.swing.JLabel lblMaxClusters;
+    private javax.swing.JLabel lblTreeDepth;
     private javax.swing.JSpinner levelSpin;
     private javax.swing.JPanel treePanel;
     private javax.swing.JButton zoomInBtn;
