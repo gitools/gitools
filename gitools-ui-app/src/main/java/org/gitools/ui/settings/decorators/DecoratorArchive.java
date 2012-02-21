63b763ab28ae51caf2662e858a869e20c46c241d
michphip <michael.p.schroeder@gmail.com>
2012-02-21 18:38:42 +0000

    new functionality: save and load decorators / scales (2)
    git-svn-id: https://bg.upf.edu/svn/gitools/desktop/trunk@1736 1b512f91-3386-4a98-81e7-b8836ddf8916

---
+++ gitools-ui-app/src/main/java/org/gitools/ui/settings/decorators/DecoratorArchive.java
@@ -0,0 +1,108 @@
+/*
+ *  Copyright 2012 michi.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.gitools.ui.settings.decorators;
+
+import java.util.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.gitools.model.decorator.ElementDecorator;
+import org.gitools.model.decorator.ElementDecoratorDescriptor;
+import org.gitools.model.decorator.ElementDecoratorFactory;
+
+
+/**
+ *
+ * @author michi
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DecoratorArchive {
+
+    private String version;
+    private List<ElementDecorator> scaleDecorators = new ArrayList<ElementDecorator>();
+        
+    private transient final String DEFAULT = "Default";
+
+    public DecoratorArchive() {
+    }
+
+    public void add(ElementDecorator[] decorators) {
+        for (ElementDecorator d : decorators) {
+            add(d);
+        }
+    }
+    
+    public void add(ElementDecorator decorator) {
+        String name = decorator.getName();
+        Set<String> map = getDecorators().keySet();
+        if (map.contains(name)) {
+            remove(scaleDecorators,name);
+            scaleDecorators.add(decorator);
+        } else {
+            scaleDecorators.add(decorator);
+        }
+    }
+    
+    private void remove(List<ElementDecorator> scaleDecorators, String name) {
+           for(ElementDecorator d : scaleDecorators) {
+               if (d.getName().equals(name)) {
+                   scaleDecorators.remove(d);
+                   return;
+               }
+           }
+    }
+    
+    public Map<String, ElementDecorator> getDecorators(){
+        Map<String, ElementDecorator> decoratorMap = new HashMap<String, ElementDecorator>();
+        
+        for (ElementDecorator d : scaleDecorators) {
+            decoratorMap.put(d.getName(), d);
+        }
+        return decoratorMap;
+    }
+   
+    public ElementDecorator[] getDefaultElementDecoratros() {
+        List<ElementDecoratorDescriptor> descriptors = ElementDecoratorFactory.getDescriptors();
+        ElementDecorator[] decorators = new ElementDecorator[descriptors.size()];
+        int i = 0;
+        for (ElementDecoratorDescriptor descriptor : descriptors) {
+            ElementDecorator decorator = null;
+            try {
+                decorator = descriptor.getDecoratorClass().
+                        getConstructor().newInstance();
+            }
+            catch (Exception e) {
+            	return null;
+            }
+            //decorator = (descriptor.getDecoratorClass()) decorator;
+            decorator.setName(DEFAULT + " " +descriptor.getName());
+            decorators[i] = decorator;
+            i++;
+        }
+        return decorators;
+    }
+
+	public String getVersion() {
+		return version;
+	}
+    
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
---
+++ gitools-ui-app/src/main/java/org/gitools/ui/settings/decorators/DecoratorArchivePersistance.java
@@ -0,0 +1,158 @@
+/*
+ *  Copyright 2012 michi.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.gitools.ui.settings.decorators;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.reflection.PureJavaReflectionProvider;
+import edu.upf.bg.colorscale.ColorScaleFragment;
+import edu.upf.bg.colorscale.ColorScalePoint;
+import edu.upf.bg.colorscale.impl.*;
+import edu.upf.bg.cutoffcmp.CutoffCmp;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import org.gitools.model.decorator.ElementDecorator;
+import org.gitools.model.decorator.ElementDecoratorDescriptor;
+import org.gitools.model.decorator.ElementDecoratorFactory;
+import org.gitools.model.decorator.impl.*;
+
+
+/**
+ *
+ * @author michi
+ */
+public class DecoratorArchivePersistance {
+
+ 
+
+    private transient static XStream xstream;
+
+
+	/*public static DecoratorArchivePersistance getInstance() {
+		if (instance == null) {
+            instance = load();
+        }
+        return instance;
+	}*/
+
+    private static final String userPath =
+		System.getProperty("user.home", ".");
+
+	public static final String CONFIG_PATH =
+		userPath + File.separator + ".gitools";
+
+	private static final String configFileName = "scales.xml";
+
+	private static final String configFile =
+		CONFIG_PATH + File.separator + configFileName;
+
+    /*	FormattedTextElementDecorator.class,
+	LinearTwoSidedElementDecorator.class,
+	PValueElementDecorator.class,
+	ZScoreElementDecorator.class,
+    CorrelationElementDecorator.class*/
+    
+    public DecoratorArchivePersistance() {
+        xstream = new XStream();
+        
+        xstream.alias("decoratorArchive", DecoratorArchive.class);
+        xstream.alias("colorScalePoint", ColorScalePoint.class);
+        
+        xstream.alias("colorScaleFragment", ColorScaleFragment.class);
+        xstream.alias("linearColorScale", LinearColorScale.class);
+        xstream.alias("uniformColorScale", UniformColorScale.class);
+        xstream.alias("logColorScale", LogColorScale.class);
+        xstream.alias("correlationColorScale", CorrelationColorScale.class);
+        
+        xstream.aliasPackage("cutoffComparison", "edu.upf.bg.cutoffcmp");
+        
+        xstream.alias("binaryDecorator", BinaryElementDecorator.class);
+        xstream.omitField(BinaryElementDecorator.class, "valueIndex");     
+        //xstream.omitField(BinaryElementDecorator.class, "fmt");  
+        
+        xstream.alias("linearTwoSidedDecorator", LinearTwoSidedElementDecorator.class);
+        xstream.omitField(LinearTwoSidedElementDecorator.class, "valueIndex");
+        //xstream.omitField(LinearTwoSidedElementDecorator.class, "fmt"); 
+        
+        xstream.alias("formattedTextDecorator", FormattedTextElementDecorator.class);
+        xstream.omitField(FormattedTextElementDecorator.class, "valueIndex");
+        //xstream.omitField(FormattedTextElementDecorator.class, "fmt"); 
+        
+        xstream.alias("pValueDecorator", PValueElementDecorator.class);
+        xstream.omitField(PValueElementDecorator.class, "valueIndex");
+        xstream.omitField(PValueElementDecorator.class, "correctedValueIndex");
+        //xstream.omitField(PValueElementDecorator.class, "fmt"); 
+        
+        xstream.alias("zScoreDecorator", ZScoreElementDecorator.class);
+        xstream.omitField(ZScoreElementDecorator.class, "valueIndex");
+        xstream.omitField(ZScoreElementDecorator.class, "correctedValueIndex");
+        //xstream.omitField(ZScoreElementDecorator.class, "fmt");
+        
+        xstream.alias("correlationDecorator", CorrelationElementDecorator.class);
+        xstream.omitField(CorrelationElementDecorator.class, "valueIndex");
+        //xstream.omitField(CorrelationElementDecorator.class, "fmt");
+        
+        xstream.omitField(ElementDecorator.class, "valueIndex");
+        xstream.omitField(ElementDecorator.class, "adapter");
+        //xstream.omitField(ElementDecorator.class, "fmt");
+        
+        xstream.setMode(XStream.NO_REFERENCES);
+    }
+   
+    public void save(DecoratorArchive archive) {
+        File path = new File(CONFIG_PATH);
+        if (!path.exists())
+            path.mkdirs();
+
+        try {
+            FileWriter writer = new FileWriter(configFile);
+            
+            xstream.toXML(archive, writer);
+            writer.close();
+            
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+    }
+
+    public DecoratorArchive load() {
+        DecoratorArchive decoratorArchive;
+        try {
+            Reader reader = new FileReader(configFile);
+            decoratorArchive = (DecoratorArchive) xstream.fromXML(reader, new DecoratorArchive());
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Settings file doesn't exist: " + configFile);
+            System.err.println("Created one with defaults.");
+            decoratorArchive = new DecoratorArchive();
+            decoratorArchive.add(decoratorArchive.getDefaultElementDecoratros());
+            save(decoratorArchive);
+        }
+        catch (Exception e) {
+            decoratorArchive = new DecoratorArchive();
+            e.printStackTrace();
+        }
+        return decoratorArchive;
+    }
+}
