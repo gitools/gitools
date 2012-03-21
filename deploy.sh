4b90dc7a66246214a1759d0d8aebe6654ba359a3
jordeu <jordi@jordeu.net>
2012-03-21 14:43:31 +0000

    Solved assembly ZIP with duplicated files and added some help how to do a release in deploy.sh comments.
    git-svn-id: https://bg.upf.edu/svn/gitools/desktop/trunk@1761 1b512f91-3386-4a98-81e7-b8836ddf8916

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-mvn package install assembly:assembly -DskipTests=true
+mvn -q package install assembly:assembly
+
+# Follow this steps to do a release
+
+# First commit
+# svn commit
+
+# To prepare the release
+# mvn release:prepare
+
+# To perform the release
+# mvn release:perform -DaltDeploymentRepository=none::default::file:target/deploy
+
 
