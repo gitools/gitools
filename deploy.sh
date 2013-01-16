489a2ee4168802fa4b91fb9ab5e8f1cb65c3eaa2
Jordi Deu-Pons <jordi@jordeu.net>
2013-01-16 17:06:31 +0100

    Remove old release stuff

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,4 @@
 
 mvn -q package install assembly:assembly
 
-# Follow this steps to do a release
-
-# First commit
-# svn commit
-
-# To prepare the release
-# mvn release:prepare
-
-# To perform the release
-# mvn release:perform -DaltDeploymentRepository=none::default::file:target/deploy
-
 
