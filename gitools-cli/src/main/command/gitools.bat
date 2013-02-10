79e1895bf5b4db3c849640dd1ab770818a388c74
Jordi Deu-Pons <jordi@jordeu.net>
2013-02-10 09:57:37 +0100

    FIX: Increase default heap space memory for Windows users. Related issue #17

--- gitools-cli/src/main/command/gitools.bat
+++ gitools-cli/src/main/command/gitools.bat
@@ -1,1 +1,1 @@
-java %GITOOLS_JAVA_OPTS% -jar ..\lib\gitools-ui-app-${project.version}.jar %*
+java -Xmx1100m -Xms256m -XX:MaxPermSize=250m -XX:+UseSerialGC -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=20 -jar ..\lib\gitools-ui-app-${project.version}.jar %*
