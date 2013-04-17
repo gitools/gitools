64eb4850bbda13903b310ec142438a7c51b77f5e
Michael P Schroeder <michael.p.schroeder@gmail.com>
2013-04-17 11:44:34 +0300

    Display whole filename of opened heatmap

--- gitools-core/src/main/java/org/gitools/persistence/locators/UrlResourceLocator.java
+++ gitools-core/src/main/java/org/gitools/persistence/locators/UrlResourceLocator.java
@@ -32,8 +32,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.regex.Pattern;
 
-public class UrlResourceLocator implements IResourceLocator
-{
+public class UrlResourceLocator implements IResourceLocator {
 
     private static final Pattern ABSOLUTE_FILE_PATH = Pattern.compile("^(\\/.*|[a-zA-Z]\\:\\\\)");
     private static final Pattern ABSOLUTE_REMOTE_URL = Pattern.compile("[a-zA-Z]+:\\/\\/.*");
@@ -42,110 +41,89 @@ public class UrlResourceLocator implements IResourceLocator
     @Nullable
     private File file;
 
-    public UrlResourceLocator(@NotNull File file) throws PersistenceException
-    {
+    public UrlResourceLocator(@NotNull File file) throws PersistenceException {
         this.file = file;
-        try
-        {
+        try {
             this.url = file.toURI().toURL();
-        } catch (MalformedURLException e)
-        {
+        } catch (MalformedURLException e) {
             throw new PersistenceException(e);
         }
     }
 
-    public UrlResourceLocator(String url) throws PersistenceException
-    {
-        try
-        {
-            if (ABSOLUTE_REMOTE_URL.matcher(url).matches())
-            {
+    public UrlResourceLocator(String url) throws PersistenceException {
+        try {
+            if (ABSOLUTE_REMOTE_URL.matcher(url).matches()) {
                 this.file = null;
                 this.url = new URL(url);
 
-                if (this.url.getProtocol().equals("file"))
-                {
+                if (this.url.getProtocol().equals("file")) {
                     this.file = new File(this.url.toURI());
                 }
 
-            }
-            else
-            {
+            } else {
                 this.file = new File(url);
                 this.url = file.toURI().toURL();
             }
-        } catch (MalformedURLException e)
-        {
+        } catch (MalformedURLException e) {
             throw new PersistenceException(e);
-        } catch (URISyntaxException e)
-        {
+        } catch (URISyntaxException e) {
             throw new PersistenceException(e);
         }
 
     }
 
     @Override
-    public URL getURL()
-    {
+    public URL getURL() {
         return url;
     }
 
     @Override
-    public String getBaseName()
-    {
+    public String getBaseName() {
         String name = getName();
 
-        int firstDot = name.indexOf('.');
-        if (firstDot != -1)
-        {
-            name = name.substring(0, firstDot);
+        int lastDot = name.lastIndexOf('.');
+        if (lastDot != -1) {
+            name = name.substring(0, lastDot);
         }
 
         return name;
     }
 
     @Override
-    public String getName()
-    {
+    public String getName() {
         String name = url.toString();
         return name.substring(name.lastIndexOf('/') + 1);
     }
 
     @Override
-    public String getExtension()
-    {
+    public String getExtension() {
         String name = getName();
         return name.substring(name.indexOf(".") + 1);
     }
 
     @Override
-    public boolean isWritable()
-    {
+    public boolean isWritable() {
         return file != null;
     }
 
     @NotNull
     @Override
-    public IResourceLocator getReferenceLocator(String referenceName) throws PersistenceException
-    {
+    public IResourceLocator getReferenceLocator(String referenceName) throws PersistenceException {
 
         // An absolute remote file
-        if (ABSOLUTE_REMOTE_URL.matcher(referenceName).matches())
-        {
+        if (ABSOLUTE_REMOTE_URL.matcher(referenceName).matches()) {
             return new UrlResourceLocator(referenceName);
         }
 
         // A relative path to a remote URL
-        if (file == null)
-        {
+        if (file == null) {
             String parentUrl = url.toString();
             parentUrl = parentUrl.substring(0, parentUrl.lastIndexOf('/'));
             return new UrlResourceLocator(parentUrl + '/' + referenceName);
         }
 
         // We allow absolute path references on local files
-        if (ABSOLUTE_FILE_PATH.matcher(referenceName).matches())
-        {
+        if (ABSOLUTE_FILE_PATH.matcher(referenceName).matches()) {
             return new UrlResourceLocator(new File(referenceName));
         }
 
@@ -155,11 +133,9 @@ public class UrlResourceLocator implements IResourceLocator
 
     @Nullable
     @Override
-    public InputStream openInputStream() throws IOException
-    {
+    public InputStream openInputStream() throws IOException {
 
-        if (file == null)
-        {
+        if (file == null) {
             return url.openStream();
         }
 
@@ -168,11 +144,9 @@ public class UrlResourceLocator implements IResourceLocator
 
     @Nullable
     @Override
-    public OutputStream openOutputStream() throws IOException
-    {
+    public OutputStream openOutputStream() throws IOException {
 
-        if (!isWritable())
-        {
+        if (!isWritable()) {
             throw new UnsupportedOperationException("Write to '" + url + "' is not supported.");
         }
 
