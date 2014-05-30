8ecea7468734513a439e2562abed73a598f3954b
Michael P Schroeder <michael.p.schroeder@gmail.com>
2014-05-30 20:15:13 +0200

    fix column selection new tip

--- org.gitools.ui.app/src/main/java/org/gitools/ui/app/heatmap/drawer/AbstractHeatmapDrawer.java
+++ org.gitools.ui.app/src/main/java/org/gitools/ui/app/heatmap/drawer/AbstractHeatmapDrawer.java
@@ -22,6 +22,7 @@
 package org.gitools.ui.app.heatmap.drawer;
 
 import org.apache.commons.lang.StringUtils;
+import org.gitools.api.matrix.MatrixDimensionKey;
 import org.gitools.heatmap.Heatmap;
 import org.gitools.heatmap.HeatmapDimension;
 import org.gitools.heatmap.decorator.Decoration;
@@ -189,7 +190,7 @@ public abstract class AbstractHeatmapDrawer {
 
         int start = -1;
         int end = -1;
-        if (drawingRows) {
+        if (dimension.getId().equals(MatrixDimensionKey.ROWS)) {
             start = this.onScreenRect.rowStart;
             end = this.onScreenRect.rowEnd;
         } else {
@@ -263,29 +264,4 @@ public abstract class AbstractHeatmapDrawer {
         return new Color((int) r, (int) g, (int) b, (int) a);
     }
 
-    Color blend(Color c1, Color c2, float ratio) {
-        if (ratio > 1f) ratio = 1f;
-        else if (ratio < 0f) ratio = 0f;
-        float iRatio = 1.0f - ratio;
-
-        int i1 = c1.getRGB();
-        int i2 = c2.getRGB();
-
-        int a1 = (i1 >> 24 & 0xff);
-        int r1 = ((i1 & 0xff0000) >> 16);
-        int g1 = ((i1 & 0xff00) >> 8);
-        int b1 = (i1 & 0xff);
-
-        int a2 = (i2 >> 24 & 0xff);
-        int r2 = ((i2 & 0xff0000) >> 16);
-        int g2 = ((i2 & 0xff00) >> 8);
-        int b2 = (i2 & 0xff);
-
-        int a = (int) ((a1 * iRatio) + (a2 * ratio));
-        int r = (int) ((r1 * iRatio) + (r2 * ratio));
-        int g = (int) ((g1 * iRatio) + (g2 * ratio));
-        int b = (int) ((b1 * iRatio) + (b2 * ratio));
-
-        return new Color(a << 24 | r << 16 | g << 8 | b);
-    }
 }
